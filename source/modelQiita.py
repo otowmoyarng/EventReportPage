f7c76a75a19883533cf301417b7e68ec62f044e8
otowmoyarng <hidemusiam@gmail.com>
2021-07-06 21:49:22 +0900

    実装完了

--- source/modelQiita.py
+++ source/modelQiita.py
@@ -1,12 +1,13 @@
 from commonlogger import getLogger, IsDebug
 from datetime import date
+from qiita_v2.client import QiitaClient
 from typing import Dict
 import json
 import requests
 
 BASE_URL = "https://qiita.com/api/v2/items"
-TOKEN = ""
-RANKING_EN = ["one","two","three","four","five"]
+RANKING_EN = ["one", "two", "three", "four", "five"]
+
 
 def PostQiita(startdate: date, enddate: date, allevents: Dict, isDebug: bool = False) -> None:
     """[summary]
@@ -20,14 +21,11 @@ def PostQiita(startdate: date, enddate: date, allevents: Dict, isDebug: bool = F
     logger = getLogger(isDebug)
 
     # アクセストークン取得
+    token = None
     with open("../qiita.token", encoding="utf-8") as f:
-        global TOKEN
-        TOKEN = f.read()
+        token = f.read()
 
-    headers = {
-        "Authorization": f'Bearer {TOKEN}',
-        "Content-Type": 'application/json'
-    }
+    client = QiitaClient(access_token=token)
 
     # リクエストボディを生成する
     with open("../templete.json", encoding="utf-8") as f:
@@ -40,11 +38,15 @@ def PostQiita(startdate: date, enddate: date, allevents: Dict, isDebug: bool = F
             break
 
         eventname = f'[{event["title"]}]({event["event_url"]})'
-        groupname = "" if event["series"] is None else f'[{event["series"]["title"]}]({event["series"]["url"]})'
+        groupname = "" if event[
+            "series"] is None else f'[{event["series"]["title"]}]({event["series"]["url"]})'
         accepted = "" if event["accepted"] is None else event["accepted"]
         limit = "" if event["limit"] is None else event["limit"]
-        body += f'\n|:{RANKING_EN[index]}:|{eventname}|{groupname}|{accepted}|{limit}|'
-        print(f'body={body}')
+        bodyline = f'\n|:{RANKING_EN[index]}:|{eventname}|{groupname}|{accepted}|{limit}|'
+        body += bodyline
+        logger.debug(
+            f'eventname={eventname}, groupname={groupname}, accepted={accepted}, limit={limit}')
+        logger.debug(f'bodyline={bodyline}')
 
     jsonbody["body"] = body
 
@@ -52,15 +54,13 @@ def PostQiita(startdate: date, enddate: date, allevents: Dict, isDebug: bool = F
     titledate = enddate.strftime('%Y/%m/%d')
     title = f'{titledate}' + title
     jsonbody["title"] = title
+    logger.debug(f'body={jsonbody}')
 
     with open(f'../json/QiitaAPI_requestbody_{enddate}.json', 'w', encoding="utf-8") as f:
         json.dump(jsonbody, f, indent=4)
 
-    requestbody = json.dumps(jsonbody)
-    print(f'requestbody={requestbody}')
-
     try:
-        res = requests.post(BASE_URL, headers=headers, json=requestbody)
-        print(f'status_code:{res.status_code}, error:{res.text}')
+        res = client.create_item(params=jsonbody)
+        print(res)
     except Exception as e:
         raise e
