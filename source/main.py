f2bd256b3bbbc41bfb4b22e6e6e29614ca36f8c1
otowmoyarng <hidemusiam@gmail.com>
2021-07-08 21:08:30 +0900

    pubsub疎通テスト用のメソッドを追加

--- source/main.py
+++ source/main.py
@@ -1,8 +1,18 @@
 from commonlogger import getLogger
 from modelConnpass import GetEventData, ReportCycle
 from modelQiita import PostQiita
+import base64
 import sys
 
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    print("pub/sub forwarded")
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
 
 def main(event=None, context=None):
 
